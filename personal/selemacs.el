8f6136ec57db301f939ecd0e3e200e4c61ceec11
Dmitry Groshev <groshev@selectel.ru>
2012-10-24 20:47:20 +0400

    TEX stuff added

--- personal/selemacs.el
+++ personal/selemacs.el
@@ -179,3 +179,23 @@ A place is considered `tab-width' character columns."
 (add-hook 'erlang-mode-hook 'run-prog-hook)
 
 (require 'erlang-start)
+
+;; LaTeX
+
+;; (require 'tex-site)
+;; (require 'preview-latex)
+
+(setq
+ TeX-auto-save t
+ TeX-parse-self t
+ TeX-DVI-via-PDFTeX t)
+
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook
+          '(lambda ()
+             (TeX-fold-mode 1)
+             (TeX-PDF-mode 1)
+             (outline-minor-mode 1)
+             (add-to-list 'TeX-command-list
+                          '("XeLaTeX" "xelatex -interaction=nonstopmode -shell-escape %s"
+                            TeX-run-command t t :help "Run xelatex") t)))
